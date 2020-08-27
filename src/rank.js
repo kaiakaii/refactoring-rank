3db2c0e36aedf1a20d85a830dbd7607f6e481835
SUNSK <senkk512@163.com>
2020-08-27 21:49:07 +0800

    [homework][Sky]refactor: delete unused const

--- src/rank.js
+++ src/rank.js
@@ -87,26 +87,3 @@ function rating(voyage, history) {
 module.exports = {
     rating, voyageRisk, hasChina, captainHistoryRisk, voyageProfitFactor
 };
-
-const voyage = {
-    zone: 'west-indies',
-    length: 10,
-};
-const history = [
-    {
-        zone: 'east-indies',
-        profit: 5,
-    }, {
-        zone: 'west-indies',
-        profit: 15,
-    }, {
-        zone: 'china',
-        profit: -2,
-    },
-    {
-        zone: 'west-africa',
-        profit: 7,
-    },
-];
-const myRating = rating(voyage, history);
-console.log(`myRating: ${myRating}`);
