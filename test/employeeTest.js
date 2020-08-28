84dd0200abc80e46e024c1a239b81a16bfbc5d1b
SUNSK <senkk512@163.com>
2020-08-28 18:58:22 +0800

    [homework][Sky]test:add should throw exception when new a Employee given name and wrong type test

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -1,6 +1,9 @@
 const employeeTest = require('ava')
 const {Employee} = require('../src/employee')
 
-employeeTest('employeeTest case1. Should not throw exception when new a Employee given name and right type', t => {
+employeeTest('employeeTest case1. should not throw exception when new a Employee given name and right type', t => {
     t.notThrows(() => new Employee('tom', 'engineer'))
+})
+employeeTest('employeeTest case2. should throw exception when new a Employee given name and wrong type', t => {
+    t.throws(() => new Employee('Kitty', 'test'))
 })
