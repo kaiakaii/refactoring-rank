19aaf49b346fbb129fac2a53b508592ecc2291ca
SUNSK <senkk512@163.com>
2020-08-28 18:44:11 +0800

    [homework][Sky]test:add deliveryDate case 2. should return date when deliveryDate given anOrder with NH state and rush test

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -18,3 +18,21 @@ deliveryTest('deliveryDate case 1. should return date when deliveryDate given an
     plusDay.setDate(today.getDate() + 2)
     t.is(deliveryDate(anOrder, rush), plusDay.toLocaleDateString());
 })
+deliveryTest('deliveryDate case 2. should return date when deliveryDate given anOrder with NH state and rush', t => {
+    const anOrder = {
+        'deliveryState': 'NH',
+        'placedOn': {
+            'plusDays': function (plusDays) {
+                let today = new Date();
+                let plusedDay = new Date();
+                plusedDay.setDate(today.getDate() + plusDays);
+                return plusedDay.toLocaleDateString();
+            }
+        }
+    }
+    const rush = true;
+    let today = new Date();
+    let plusDay = new Date();
+    plusDay.setDate(today.getDate() + 3)
+    t.is(deliveryDate(anOrder, rush), plusDay.toLocaleDateString());
+})
