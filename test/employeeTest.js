e685d9e706781aa2af94ace8f33dec8ad0d96a34
SUNSK <senkk512@163.com>
2020-08-28 18:59:29 +0800

    [homework][Sky]test:add should return name and type when toString given employee test

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -7,3 +7,7 @@ employeeTest('employeeTest case1. should not throw exception when new a Employee
 employeeTest('employeeTest case2. should throw exception when new a Employee given name and wrong type', t => {
     t.throws(() => new Employee('Kitty', 'test'))
 })
+employeeTest('employeeTest case3. should return name and type when toString given employee', t => {
+    const employee = new Employee('jack', 'engineer')
+    t.is(employee.toString(), 'jack (engineer)')
+})
